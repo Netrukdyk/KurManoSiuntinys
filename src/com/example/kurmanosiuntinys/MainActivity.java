357817301f0c03fa4fce5e27445b3ae6cd0ac7ae
Netrukdyk <bekraujis@gmail.com>
2015-01-02 15:44:34 +0200

    Updated

--- src/com/example/kurmanosiuntinys/MainActivity.java
+++ src/com/example/kurmanosiuntinys/MainActivity.java
@@ -6,6 +6,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.lang.reflect.Field;
 import java.net.URI;
+import java.util.Iterator;
+import java.util.List;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -14,6 +16,7 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.safety.Whitelist;
 import org.jsoup.select.Elements;
 
 import android.annotation.SuppressLint;
@@ -34,14 +37,12 @@ import android.widget.TextView;
 public class MainActivity extends Activity implements OnClickListener {
 
 	private TextView in;
+	String list[] = {"RC313227871HK", "RN037964246LT"};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-		in = (TextView) findViewById(R.id.in);
-		// out = (TextView) findViewById(R.id.out);
 
 		getOverflowMenu();
 	}
@@ -49,8 +50,7 @@ public class MainActivity extends Activity implements OnClickListener {
 	private void getOverflowMenu() {
 		try {
 			ViewConfiguration config = ViewConfiguration.get(this);
-			Field menuKeyField = ViewConfiguration.class
-					.getDeclaredField("sHasPermanentMenuKey");
+			Field menuKeyField = ViewConfiguration.class.getDeclaredField("sHasPermanentMenuKey");
 			if (menuKeyField != null) {
 				menuKeyField.setAccessible(true);
 				menuKeyField.setBoolean(config, false);
@@ -70,15 +70,15 @@ public class MainActivity extends Activity implements OnClickListener {
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
-		case R.id.action_about:
-			Intent intent = new Intent(this, ActivityAbout.class);
-			startActivity(intent);
-			return true;
-		case R.id.action_refresh:
-			new Tikrinti().execute("RT123456789LT", "CN123456789LT");
-			return true;
-		default:
-			return super.onOptionsItemSelected(item);
+			case R.id.action_about :
+				Intent intent = new Intent(this, ActivityAbout.class);
+				startActivity(intent);
+				return true;
+			case R.id.action_refresh :
+				new Tikrinti().execute(list);
+				return true;
+			default :
+				return super.onOptionsItemSelected(item);
 		}
 	}
 
@@ -98,6 +98,7 @@ public class MainActivity extends Activity implements OnClickListener {
 			out.setText("Tikrinama...");
 		}
 
+		@SuppressWarnings("unchecked")
 		@SuppressLint("UseValueOf")
 		protected String doInBackground(String... numbers) {
 			InputStream in = null;
@@ -106,35 +107,65 @@ public class MainActivity extends Activity implements OnClickListener {
 			String result = "";
 			for (int i = 1; i < count; i++) {
 				query += "%0D%0A" + numbers[i];
+				Log.v("Query", query);
 			}
 			try {
 				HttpClient httpclient = new DefaultHttpClient();
 				HttpGet request = new HttpGet();
-				URI website = new URI(
-						"http://www.post.lt/lt/pagalba/siuntu-paieska/index?num="
-								+ query);
+				URI website = new URI("http://www.post.lt/lt/pagalba/siuntu-paieska/index?num=" + query);
 				request.setURI(website);
 				HttpResponse response = httpclient.execute(request);
 				in = response.getEntity().getContent();
-				
-		// ---------  Parse HTML ----------------------------------------
+
+				// --------- Parse HTML ----------------------------------------
 				String html = convertInputStreamToString(in);
 				Document doc = Jsoup.parse(html);
-				
-				Elements data = doc.getElementsByClass("result"); // ieðkom rezultatø
-				if(data.isEmpty()) data = doc.getElementsByClass("notfound");
-				
-				for (Element number:data) {
-					result += number.text()+"\n\n";
-					number.hasText();
-					//number.ha
-					/* Neteisingas siuntos numerio formatas
-					 * Pagal pateiktà siuntos numerá, duomenø rasti nepavyko.
-					 * Table ... 
-					 */
+
+				// surenka su error
+				// Elements errors = doc.getElementsByClass("notfound");
+				// for (Element number : errors) {
+				// result += number.text() + "\n\n";
+				// }
+				// Log.v("Table", doc.html());
+
+				Elements tables = doc.select("table");
+				Log.v("Num", tables.size() + "x");
+				if (tables.size() > 0) {
+					for (Element table : tables) {
+						String number = table.getElementsByTag("strong").text();
+						Log.v("Number", number);
+						result += number+"\n";
+						for (Element row : table.select("tr")) {
+							Elements tds = row.select("td");
+							if (tds.size() > 2) {								
+								result += tds.last().text() + " : "+tds.get(1).text()+ "\n";
+							}
+						}
+						result += "\n\n";
+					}
 				}
-				
-		// --------- End Parse HTML ----------------------------------------
+
+				// result = String.valueOf(data.isEmpty());
+				// for(Element elem : data){
+				// elem.toString();
+				// result += elem.text() + "\n\n";
+				// }
+
+				// if(data.isEmpty()){ // ERROR
+				// data = doc.getElementsByClass("notfound");
+				// for (Element number:data) {
+				// result += number.text()+"\n\n";
+				// number.hasText();
+				// //number.ha
+				// /* Neteisingas siuntos numerio formatas
+				// * Pagal pateiktà siuntos numerá, duomenø rasti nepavyko.
+				// * Table ...
+				// */
+				// }
+				// }
+
+				// --------- End Parse HTML
+				// ----------------------------------------
 				// publishProgress(text); // rodyti kai tikrina
 
 			} catch (Exception e) {
@@ -143,10 +174,8 @@ public class MainActivity extends Activity implements OnClickListener {
 			return result;
 		}
 
-		private String convertInputStreamToString(InputStream inputStream)
-				throws IOException {
-			BufferedReader bufferedReader = new BufferedReader(
-					new InputStreamReader(inputStream));
+		private String convertInputStreamToString(InputStream inputStream) throws IOException {
+			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
 			String line = "";
 			String result = "";
 			while ((line = bufferedReader.readLine()) != null)
